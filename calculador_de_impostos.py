5b4a165c7385420ccd4c4ed2e72419fc8c149419
LivioAlvarenga <68560929+LivioAlvarenga@users.noreply.github.com>
2021-06-09 09:22:55 -0300

    Sem If - funções como atributo - Padrão Strategy

--- calculador_de_impostos.py
+++ calculador_de_impostos.py
@@ -4,13 +4,9 @@ from impostos import calcula_ICMS, calcula_ISS
 
 class Calculador_de_impostos(object):
 
-    def realiza_calculo(self, orçamento: Orcamento, imposto: str) -> None:
+    def realiza_calculo(self, orçamento: Orcamento, imposto) -> None:
 
-        if imposto == 'ICMS':
-            imposto_calculado = calcula_ICMS(orçamento)
-
-        elif imposto == 'ISS':
-            imposto_calculado = calcula_ISS(orçamento)
+        imposto_calculado = imposto(orçamento)
 
         print(imposto_calculado)
 
@@ -21,5 +17,7 @@ if __name__ == '__main__':
 
     orcamento = Orcamento(500.0)
     calculador_de_impostos = Calculador_de_impostos()
-    calculador_de_impostos.realiza_calculo(orcamento, 'ICMS')  # imprime 50.0
-    calculador_de_impostos.realiza_calculo(orcamento, 'ISS')  # imprime 30.0
+    calculador_de_impostos.realiza_calculo(
+        orcamento, calcula_ICMS)  # imprime 50.0
+    calculador_de_impostos.realiza_calculo(
+        orcamento, calcula_ISS)  # imprime 30.0
