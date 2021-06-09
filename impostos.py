802cd2e8220bb2a85b174eadedacb0466feb9920
LivioAlvarenga <68560929+LivioAlvarenga@users.noreply.github.com>
2021-06-09 17:26:36 -0300

    Aplicando padrao Template Method em IKVC e ICPP

--- impostos.py
+++ impostos.py
@@ -1,4 +1,28 @@
 from orcamento import Orcamento
+from abc import ABCMeta, abstractmethod
+
+
+class Template_de_imposto_condicional(object):
+
+    __metaclass__ = ABCMeta
+
+    def calcula(self, orçamento: Orcamento):
+        if self.deve_usar_maxima_taxacao(orçamento):
+            return self.maxima_taxacao(orçamento)
+        else:
+            return self.minima_taxacao(orçamento)
+
+    @abstractmethod
+    def deve_usar_maxima_taxacao(self, orcamento: Orcamento) -> bool:
+        pass
+
+    @abstractmethod
+    def maxima_taxacao(self, orcamento: Orcamento) -> float:
+        pass
+
+    @abstractmethod
+    def minima_taxacao(self, orcamento: Orcamento) -> float:
+        pass
 
 
 class ISS(object):
@@ -15,24 +39,30 @@ class ICMS(object):
         return orçamento.valor * 0.06
 
 
-class ICPP(object):
+class ICPP(Template_de_imposto_condicional):
 
-    def calcula(self, orçamento: Orcamento) -> float:
-        if orçamento.valor > 500:
-            return orçamento.valor * 0.07
-        else:
-            return orçamento.valor * 0.05
+    def deve_usar_maxima_taxacao(self, orçamento) -> bool:
+        return orçamento.valor > 500
+
+    def maxima_taxacao(self, orçamento) -> float:
+        return orçamento.valor * 0.07
+
+    def minima_taxacao(self, orçamento) -> float:
+        return orçamento.valor * 0.05
 
 
-class IKCV(object):
+class IKCV(Template_de_imposto_condicional):
 
-    def calcula(self, orçamento: Orcamento) -> float:
-        if orçamento.valor > 500 and (
+    def deve_usar_maxima_taxacao(self, orçamento) -> bool:
+        return orçamento.valor > 500 and (
             self.__tem_item_maior_que_100_reais(orçamento)
-        ):
-            return orçamento.valor * 0.10
-        else:
-            return orçamento.valor * 0.06
+        )
+
+    def maxima_taxacao(self, orçamento) -> float:
+        return orçamento.valor * 0.10
+
+    def minima_taxacao(self, orçamento) -> float:
+        return orçamento.valor * 0.06
 
     def __tem_item_maior_que_100_reais(self, orçamento: Orcamento) -> bool:
         for item in orçamento.obter_itens():
