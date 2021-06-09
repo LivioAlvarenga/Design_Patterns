7c1912a33680a78b4ff43924b3575ca1b95e0cd2
LivioAlvarenga <68560929+LivioAlvarenga@users.noreply.github.com>
2021-06-09 16:10:14 -0300

    Descontos em cadeia - Chain of Responsibility

--- calculador_de_descontos.py
+++ calculador_de_descontos.py
@@ -1,6 +1,8 @@
 from orcamento import Orcamento, Item
 from descontos import (
-    Desconto_por_cinco_itens, Desconto_por_mais_de_quinhentos_reais
+    Desconto_por_cinco_itens,
+    Desconto_por_mais_de_quinhentos_reais,
+    Sem_desconto
 )
 
 
@@ -8,11 +10,10 @@ class Calculador_de_descontos(object):
 
     def calcula(self, orçamento: Orcamento) -> float:
 
-        desconto = Desconto_por_cinco_itens().calcula(orçamento)
-        if desconto == 0:
-            desconto = (
-                Desconto_por_mais_de_quinhentos_reais().calcula(orçamento)
-            )
+        desconto = Desconto_por_cinco_itens(
+            Desconto_por_mais_de_quinhentos_reais(Sem_desconto())
+        ).calcula(orçamento)
+
         return desconto
 
 
@@ -21,7 +22,10 @@ if __name__ == '__main__':
     orcamento = Orcamento()
     orcamento.adiciona_item(Item('ITEM - 1', 100))
     orcamento.adiciona_item(Item('ITEM - 2', 50))
-    orcamento.adiciona_item(Item('ITEM - 3', 400))
+    orcamento.adiciona_item(Item('ITEM - 3', 40))
+    orcamento.adiciona_item(Item('ITEM - 4', 100))
+    orcamento.adiciona_item(Item('ITEM - 5', 100))
+    orcamento.adiciona_item(Item('ITEM - 6', 100))
 
     print(f'Valor total do orçamento {orcamento.valor}')
 
--- descontos.py
+++ descontos.py
@@ -1,19 +1,32 @@
+from typing import Literal
 from orcamento import Orcamento
 
 
 class Desconto_por_cinco_itens(object):
 
+    def __init__(self, proximo_desconto) -> None:
+        self.__proximo_desconto = proximo_desconto
+
     def calcula(self, orçamento: Orcamento) -> float:
         if orçamento.total_itens > 5:
             return orçamento.valor * 0.1
         else:
-            return 0
+            return self.__proximo_desconto.calcula(orçamento)
 
 
 class Desconto_por_mais_de_quinhentos_reais(object):
+
+    def __init__(self, proximo_desconto) -> None:
+        self.__proximo_desconto = proximo_desconto
 
     def calcula(self, orçamento: Orcamento) -> float:
         if orçamento.valor > 500:
             return orçamento.valor * 0.07
         else:
-            return 0
+            return self.__proximo_desconto.calcula(orçamento)
+
+
+class Sem_desconto(object):
+
+    def calcula(self, orçamento: Orcamento) -> Literal[0]:
+        return 0
