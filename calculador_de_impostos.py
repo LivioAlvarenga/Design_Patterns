aa4223b443e79d20fc92ae8753f76f9e2bc773db
LivioAlvarenga <68560929+LivioAlvarenga@users.noreply.github.com>
2021-06-09 09:33:44 -0300

    class como atributo - Aplicado Duck Typing

--- calculador_de_impostos.py
+++ calculador_de_impostos.py
@@ -1,12 +1,12 @@
 from orcamento import Orcamento
-from impostos import calcula_ICMS, calcula_ISS
+from impostos import ICMS, ISS
 
 
 class Calculador_de_impostos(object):
 
     def realiza_calculo(self, orçamento: Orcamento, imposto) -> None:
 
-        imposto_calculado = imposto(orçamento)
+        imposto_calculado = imposto.calcula(orçamento)
 
         print(imposto_calculado)
 
@@ -17,7 +17,5 @@ if __name__ == '__main__':
 
     orcamento = Orcamento(500.0)
     calculador_de_impostos = Calculador_de_impostos()
-    calculador_de_impostos.realiza_calculo(
-        orcamento, calcula_ICMS)  # imprime 50.0
-    calculador_de_impostos.realiza_calculo(
-        orcamento, calcula_ISS)  # imprime 30.0
+    calculador_de_impostos.realiza_calculo(orcamento, ICMS())  # imprime 50.0
+    calculador_de_impostos.realiza_calculo(orcamento, ISS())  # imprime 30.0
