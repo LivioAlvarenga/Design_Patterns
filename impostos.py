749e692430ca094c7aff51c2ca3073ca8dc19213
LivioAlvarenga <68560929+LivioAlvarenga@users.noreply.github.com>
2021-06-09 11:40:05 -0300

    merge impostos.py

--- impostos.py
+++ impostos.py
@@ -1,28 +1,13 @@
-<<<<<<< HEAD
-from orcamento import Item
+from orcamento import Orcamento
 
 
 class ISS(object):
-    def calcula(self, orçamento: Item) -> float:
+    def calcula(self, orçamento: Orcamento) -> float:
 
         return orçamento.valor * 0.1
 
 
 class ICMS(object):
-    def calcula(self, orçamento: Item) -> float:
+    def calcula(self, orçamento: Orcamento) -> float:
 
         return orçamento.valor * 0.06
-=======
-from orcamento import Orcamento
-from typing import Union
-
-
-def calcula_ISS(orçamento: Orcamento) -> Union[int, float]:
-
-    return orçamento.valor * 0.1
-
-
-def calcula_ICMS(orçamento: Orcamento) -> Union[int, float]:
-
-    return orçamento.valor * 0.06
->>>>>>> 184f6d56bf7fe8ab2316ff7c95e5ead08f48c4a5
